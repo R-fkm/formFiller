50c44ed56cabaf249c4a8a76fb6cce72b4447dc3
Ryuto Fukuma <ryutokappa1025@gmail.com>
2023-03-07 07:35:46 +0000

    htmlのitem取得 inputだけ取得するように変更する

--- content_script.js
+++ content_script.js
@@ -1,5 +1,29 @@
-let form_items = document.querySelectorAll(".form__item");
+
+
+console.log(document);
+
+//let form_items = document.querySelectorAll(".form__item");
+let form_items = document.getElementsByClassName("form__item");
 for(let i = 0; i < form_items.length; i++){
-  let e = form_items[i];
-  e.style.color = 'red';
+  let form_row = form_items[i];
+  form_row.style.color = 'red';
+
+  console.log(form_row);
+  console.log(form_row.getElementsByClassName("form__item__title")[0].innerHTML);
+ 
+
+  form_row_title = form_row.getElementsByClassName("form__item__title")[0].innerHTML;
+  form_group = form_row.getElementsByClassName("form__item__group");
+  console.log(form_group)
+  console.log('start_iter');
+  for(let j=0; j < form_group.length; j++){
+    let form_parts = form_group[j];
+    console.log('form_parts');
+    console.log(form_parts);
+  }
+  console.log('end_iter');
+
+  //console.log(form.keys(form.keys(), form.values()));
 }
+
+
